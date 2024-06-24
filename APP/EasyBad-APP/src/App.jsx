194215423e4691630080f0c157b9d44d95275a41
TigrouForMatic <jules.chassany@gmail.com>
2024-06-24 18:11:17 +0200

    Ajout de la logique mobile

--- APP/EasyBad-APP/src/App.jsx
+++ APP/EasyBad-APP/src/App.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './App.css'
 import './styles/navbarStyles.css'; 
-import SideBarContainer  from './components/Menu/SideBarContainer'
-
+import SideBarContainer from './components/Menu/SideBarContainer';
+import SideBarContainerMobile from './components/Menu/SideBarContainerMobile';
 
 function App() {
-  
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth < 768);
+    }
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <SideBarContainer/>
+      {isMobile && <SideBarContainerMobile /> ||  <SideBarContainer />}
     </>
   )
 }
 
-export default App
+export default App;
---
+++ APP/EasyBad-APP/src/components/Menu/SideBarContainerMobile.jsx
@@ -0,0 +1,14 @@
+// import Sidebar from './Sidebar';
+// import SidebarSmall from './SidebarSmall';
+
+function SidebarContainer() {
+  
+
+  return (
+    <div > 
+      <h1>C'est la side BAr mobile ouuuuuu</h1>
+    </div>
+  );
+}
+
+export default SidebarContainer;
