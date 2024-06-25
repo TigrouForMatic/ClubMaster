b6a3ab6e08357025e89543c50de0a5097ef1a432
TigrouForMatic <jules.chassany@gmail.com>
2024-06-26 01:29:55 +0200

    Ajout fichier date.js et ajout données BDD et ajot affichage home.vue

--- APP/EasyBad-APP/src/views/HomeView.jsx
+++ APP/EasyBad-APP/src/views/HomeView.jsx
@@ -1,8 +1,61 @@
-function HomeView (){
-    return(
-    <div className="">
-        <h1>HomeView</h1>
-    </div>
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {dateFormat} from "../js/date";
+
+function HomeView() {
+    const [typeEvent, setTypeEvent] = useState([]);
+    const [event, setEvent] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const [typeEventResponse, eventResponse] = await Promise.all([
+                    axios.get("http://localhost:3200/eventType?clubid=1"),
+                    axios.get("http://localhost:3200/event"),
+                ]);
+                setTypeEvent(typeEventResponse.data);
+
+                const eventsDuClub = eventResponse.data.filter(event =>
+                    typeEventResponse.data.some(type => event.eventtypeid === type.id)
+                );
+
+                setEvent(eventsDuClub);
+            } catch (error) {
+                console.error("Erreur lors de la récupération des données :", error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    const getDateDisplay = (date) => {
+        return dateFormat(date);
+    }
+
+    return (
+        <div>
+            <h1>HomeView</h1>
+
+            <h2>Types d'evenement :</h2>
+            <ul>
+                {typeEvent.map((type) => (
+                    <li key={type.id}>
+                        <p>Nom : {type.label}</p>
+                    </li>
+                ))}
+            </ul>
+
+            <h2>Evenement :</h2>
+            <ul>
+                {event.map((event) => (
+                    <li key={event.id}>
+                        <p>Nom : {event.label}</p>
+                        <p>Description : {event.description}</p>
+                        <p>Date de debut : {getDateDisplay(event.dd)}</p>
+                        <p>Date de fin : {getDateDisplay(event.df)}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
 
