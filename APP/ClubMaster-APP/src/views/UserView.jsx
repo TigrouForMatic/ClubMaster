89ea5ad036b5304778f1e33cb3b5147a93ef62a3
TigrouForMatic <jules.chassany@gmail.com>
2024-11-27 15:54:18 +0100

    Prepa dev V2 Profil

--- APP/ClubMaster-APP/src/views/UserView.jsx
+++ APP/ClubMaster-APP/src/views/UserView.jsx
@@ -84,16 +84,20 @@ function UserView() {
 
       <MenuSection />
 
+      <section className={styles.membershipInfo}>
+        <h2>Entrainement et Diététique</h2>
+      </section>
+
       <button className={styles.chatbotButton}>CHATBOT & SERVICE</button>
 
       <section className={styles.membershipInfo}>
         <h2>Mes Cartes et Réductions</h2>
-        <div className={styles.levelInfo}>
+        {/* <div className={styles.levelInfo}>
           <p>Adidas - 10%</p>
           <p>Decathlon -30%</p>
         </div>
         <ProgressBar value={500} max={1000} />
-        <p>1 month left to enjoy</p>
+        <p>1 month left to enjoy</p> */}
       </section>
 
       <button className={styles.logoutButton} onClick={handleLogout}>
