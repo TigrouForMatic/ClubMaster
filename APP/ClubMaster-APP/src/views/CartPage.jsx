124e687d0a73b310223ae0c89b1f854f984c2492
TigrouForMatic <jules.chassany@gmail.com>
2025-02-07 20:32:03 +0100

    Enh opti Front style

--- APP/ClubMaster-APP/src/views/CartPage.jsx
+++ APP/ClubMaster-APP/src/views/CartPage.jsx
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import useStore from '../store/store';
-import styles from "../styles/CartPage.module.css";
 import CustomConfirm from '../components/CustomConfirm';
 import CartHeader from '../components/Cart/CartHeader';
 import CartItemList from '../components/Cart/CartItemList';
@@ -59,38 +58,57 @@ function CartPage() {
   };
 
   return (
-    <div className={styles.cartPageContainer}>
-      <CartHeader onUndo={() => navigate('/shop')} />
-      <div className={styles.productList}>
-        <CartItemList 
-          panier={panier}
-          onRemoveItem={handleRemoveItem}
-          onQuantityChange={(id, newQuantity) => {
-            if (newQuantity === 0) {
-              handleRemoveItem(id);
-            } else {
-              updateItem('panier', id, { quantity: newQuantity });
-            }
-          }}
+    <div className="min-h-screen bg-gray-50 py-8">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mb-8">
+          <CartHeader onUndo={() => navigate('/shop')} />
+        </div>
+
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-12">
+          <div className="space-y-6 lg:col-span-8">
+            <div className="rounded-lg bg-white p-6 shadow-sm">
+              <CartItemList 
+                panier={panier}
+                onRemoveItem={handleRemoveItem}
+                onQuantityChange={(id, newQuantity) => {
+                  if (newQuantity === 0) {
+                    handleRemoveItem(id);
+                  } else {
+                    updateItem('panier', id, { quantity: newQuantity });
+                  }
+                }}
+              />
+            </div>
+
+            <div className="rounded-lg bg-white p-6 shadow-sm">
+              <AddressSection 
+                currentUserAddresses={currentUserAddresses}
+                selectedAddressId={selectedAddressId}
+                onAddressSelect={setSelectedAddressId}
+                currentUser={currentUser}
+                addItem={addItem}
+              />
+            </div>
+
+            <div className="rounded-lg bg-white p-6 shadow-sm">
+              <PaymentSection />
+            </div>
+          </div>
+
+          <div className="lg:col-span-4">
+            <div className="sticky top-4">
+              <OrderSummary panier={panier} />
+            </div>
+          </div>
+        </div>
+
+        <CustomConfirm
+          isOpen={isConfirmOpen}
+          message={confirmMessage}
+          onConfirm={handleConfirm}
+          onCancel={handleCancel}
         />
-        <hr className={styles.separator} />
-        <AddressSection 
-          currentUserAddresses={currentUserAddresses}
-          selectedAddressId={selectedAddressId}
-          onAddressSelect={setSelectedAddressId}
-          currentUser={currentUser}
-          addItem={addItem}
-        />
-        <hr className={styles.separator} />
-        <PaymentSection />
       </div>
-      <OrderSummary panier={panier} />
-      <CustomConfirm
-        isOpen={isConfirmOpen}
-        message={confirmMessage}
-        onConfirm={handleConfirm}
-        onCancel={handleCancel}
-      />
     </div>
   );
 }
