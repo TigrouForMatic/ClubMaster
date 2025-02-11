a701229167355c9c029869817a74c0e19fd16f56
TigrouForMatic <jules.chassany@gmail.com>
2025-02-11 22:30:16 +0100

    Database patch

--- API/database.js
+++ API/database.js
@@ -6,6 +6,10 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT || 5432,
+    ssl: {
+        rejectUnauthorized: false,
+        require: true
+    }
 });
 
 const setupDatabase = async () => {
@@ -14,6 +18,12 @@ const setupDatabase = async () => {
         console.log('Connexion à la base de données établie avec succès');
     } catch (err) {
         console.error('Erreur lors de la connexion à la base de données:', err);
+        console.error('Configuration de connexion:', {
+            host: process.env.DB_HOST,
+            database: process.env.DB_NAME,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+        });
     }
 };
 
