1a73fdb21dd62a641002c1fa2ff1f4fd79f3f30b
TigrouForMatic <jules.chassany@gmail.com>
2025-02-12 22:40:03 +0100

    Test variable api connect supabase

--- API/supabase.js
+++ API/supabase.js
@@ -2,7 +2,7 @@ const { createClient } = require('@supabase/supabase-js')
 
 const setupSupabase = () => {
     const supabaseUrl = process.env.SUPABASE_URL
-    const supabaseKey = process.env.SUPABASE_KEY
+    const supabaseKey = process.env.SUPABASE_ANON_KEY
     
     const supabase = createClient(supabaseUrl, supabaseKey)
     
